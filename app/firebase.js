4579d30bc9c0cc06eaa167246ec4b433214cd9aa
Paulo Ricardo S da S de Sousa <37557366+Engcompaulo@users.noreply.github.com>
2019-02-08 08:46:36 -0200

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -1,8 +1,8 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyA6eyWh-oJPMZ12Z0B47q5jaCJZKDdUqFE",
-  authDomain: "repositoriolinks.firebaseapp.com",
-  databaseURL: "https://repositoriolinks.firebaseio.com",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
   projectId: "repositoriolinks",
   storageBucket: "repositoriolinks.appspot.com",
   messagingSenderId: "811083567163"
